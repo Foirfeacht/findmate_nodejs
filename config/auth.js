b64746d7df0e3f4bf2e40dd81ad4eaa77b8b36c8
foirfeacht <vitaliy.maltsev@gmail.com>
2015-03-12 01:10:43 +0300

    config changes

--- config/auth.js
+++ config/auth.js
@@ -3,10 +3,22 @@
 // expose our config directly to our application using module.exports
 module.exports = {
 
-	'facebookAuth' : {
-		'clientID' 		: '1556663711272087', // your App ID
-		'clientSecret' 	: '6b16723e16c35fa758f97baa92dcb720', // your App Secret
-		'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback',
-		//'profileFields' : ['user_friends']
+	development: {
+
+		'facebookAuth' : {
+			'clientID' 		: '1556663711272087', // your App ID
+			'clientSecret' 	: '6b16723e16c35fa758f97baa92dcb720', // your App Secret
+			'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback'
+			//'profileFields' : ['user_friends']
+		}
+	},
+
+	production: {
+		'facebookAuth' : {
+			'clientID' 		: '1556663711272087', // your App ID
+			'clientSecret' 	: '6b16723e16c35fa758f97baa92dcb720', // your App Secret
+			'callbackURL' 	: 'https://findmates-demo.herokuapp.com/auth/facebook/callback'
+			//'profileFields' : ['user_friends']
+		}
 	}
 };
--- config/database.js
+++ config/database.js
@@ -1,6 +1,15 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://localhost:27017/findmate' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	development: {
+
+		'url' : 'mongodb://localhost:27017/findmate' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	},
+
+	production: {
+		'url' : 'mongodb:heroku_app34769764:vitmal1991@ds033897.mongolab.com:33897/heroku_app34769764'
+	}
+	// heroku mongolab db
+	//'url' : 'mongodb:heroku_app34769764:vitmal1991@ds033897.mongolab.com:33897/heroku_app34769764' 
 
 };
