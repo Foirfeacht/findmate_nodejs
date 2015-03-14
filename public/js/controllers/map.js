417b653c4f4b0e035071c6830c841f77f4c85469
foirfeacht <vitaliy.maltsev@gmail.com>
2015-03-14 22:25:03 +0300

    map refresh on modal close

--- public/js/controllers/map.js
+++ public/js/controllers/map.js
@@ -243,7 +243,45 @@ findMate.controller('mapController', ['$scope', '$http', 'mapService', '$mdSiden
             .error(function (data) {
                 console.log('Error: ' + data);
             })
-    }    
+    } 
+
+    // get function to refresh on modal closing
+
+    $scope.refresh = function() {
+        $http.get('../api/meetings')
+            .success(function(data) {
+                $scope.meetings = data;
+                console.log(data);
+
+                // draw markers from database
+                var l = data.length;
+                for( var i = 0; i < l; i++) {
+                    var latLng = new google.maps.LatLng(data[i].latitude, data[i].longitude);
+                    var marker = new google.maps.Marker({
+                        position: latLng,
+                        map: $scope.map
+                    });
+                    $scope.markers.push(marker);
+
+                    console.log(data[i].location + '\n' + data[i].title + '\n' + data[i].description);
+
+
+                    var infoContent = '<div id="content">' + data[i].location + '<br>' + data[i].title + '<br>' + 
+                                        data[i].description + '</div>';
+
+                    var infowindow = new google.maps.InfoWindow({
+                      content: infoContent
+                    });
+
+                    google.maps.event.addListener(marker, 'click', function() {
+                       infowindow.open($scope.map, marker);
+                    });
+                }
+            })
+            .error(function (data) {
+                console.log('Error: ' + data);
+            });
+    }   
 
     // side nav
     $scope.toggleNav = function() {
@@ -252,12 +290,16 @@ findMate.controller('mapController', ['$scope', '$http', 'mapService', '$mdSiden
 
 
     // md dialog
-    $scope.showDialog = function(ev) {
+    $scope.showDialog = function(ev){
         $mdDialog.show({
           controller: 'DialogController',
           templateUrl: './public/partials/dialog.tmpl.ejs',
           targetEvent: ev
-        })
-    };
-   
+             }).then(function(data) {
+                  $scope.refresh();
+                  console.log('refreshed')
+             }, function() {
+                  $scope.refresh();
+             })     
+    }
 }]);
