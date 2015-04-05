d500f429a535d1f4ec066ed159d682a7fd769e53
foirfeacht <vitaliy.maltsev@gmail.com>
2015-04-05 03:15:08 +0300

    vk oauth test

--- config/auth.js
+++ config/auth.js
@@ -6,16 +6,11 @@ module.exports = {
 		'facebookAuth' : {
 			'clientID' 		: '1556663711272087', // your App ID
 			'clientSecret' 	: '6b16723e16c35fa758f97baa92dcb720', // your App Secret
-<<<<<<< HEAD
-			'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback'
-			//'callbackURL' 	: 'https://findmates-demo.herokuapp.com/auth/facebook/callback'
+			//'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback'
+			'callbackURL' 	: 'https://findmates-demo.herokuapp.com/auth/facebook/callback'
 		},
-=======
-			'callbackURL' 	: 'https://findmates-demo.herokuapp.com/auth/facebook/callback' || 'http://localhost:8080/auth/facebook/callback'
-			//'profileFields' : ['user_friends']
-		}
 	//},
->>>>>>> 2c9e4519cbf665a63b051af072567a269cc184c2
+
 
 		'vkontakteAuth' : {
 			'clientID' 		: '4861186', // your App ID
