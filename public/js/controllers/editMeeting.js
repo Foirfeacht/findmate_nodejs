914a97c1996cc21d5aed849a7b7094c07717df03
Foirfeacht <vitaliy.maltsev@gmail.com>
2015-04-07 18:22:54 +0300

    put try 2

--- public/js/controllers/editMeeting.js
+++ public/js/controllers/editMeeting.js
@@ -186,7 +186,7 @@ findMate.controller('EditMeetingController', ['$scope', '$http', 'editService', 
   $scope.formData.invitedUsers = $scope.invitedUsers;
 
   $scope.saveMeeting = function(id) {
-      $http.put('../api/meetings' + id, $scope.formData)
+      $http.put('../api/meetings/' + id, $scope.formData)
               .success(function (data) {
                   console.log($scope.formData);
                   $scope.formData = {}; // clear the form so our user is ready to enter another
