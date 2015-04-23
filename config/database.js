37f98a744ee5837f5c5c159e703225411ce30ba9
foirfeacht <vitaliy.maltsev@gmail.com>
2015-04-24 02:01:14 +0300

    v 0.4.6

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 module.exports = {
 
-	//'url' : 'mongodb://admin:vitmal1991@ds033897.mongolab.com:33897/heroku_app34769764' || 'mongodb://localhost:27017/findmate'
-	'url' : 'mongodb://localhost:27017/findmate'
+	'url' : 'mongodb://admin:vitmal1991@ds033897.mongolab.com:33897/heroku_app34769764' || 'mongodb://localhost:27017/findmate'
+	//'url' : 'mongodb://localhost:27017/findmate'
 
 };
