ecd9c2360013e8ec36777af9cbff652363438758
Foirfeacht <vitaliy.maltsev@gmail.com>
2015-04-24 13:27:15 +0300

    usersnap

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@
 module.exports = {
 
 	'url' : 'mongodb://admin:vitmal1991@ds033897.mongolab.com:33897/heroku_app34769764' || 'mongodb://localhost:27017/findmate'
-	///'url' : 'mongodb://localhost:27017/findmate'
+	//'url' : 'mongodb://localhost:27017/findmate'
 
 };
